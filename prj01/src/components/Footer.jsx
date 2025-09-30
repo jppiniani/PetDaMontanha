985e0894662c7c2dbf527d2b23d9bd9d9cc03029
cavalcante-l <l.cavalcante@aluno.ifsp.edu.br>
2025-09-30 16:51:20 -0300

    Footer sem css

---
+++ prj01/src/components/Footer.jsx
@@ -0,0 +1,32 @@
+export default function Footer (){
+    return(
+        <>
+            <div>
+                <h2>Lambda</h2>
+                <p>A creche veterinária onde cada lambida é um gesto de carinho!</p>
+            </div>
+            
+            <div>
+                <h2>Navegação Rápida</h2>
+                <ul>
+                    <li>Início</li>
+                    <li>Sobre</li>
+                    <li>Serviços</li>
+                    <li>Planos</li>
+                    <li>Contato</li>
+                    <li>Mini-Game</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Alguma Dúvida?</h2>
+                <div>
+                    <ul>
+                        <li>Rua Falsa Lambida 203, Bragança Paulista, São Paulo</li>
+                        <li>+55 11 97226-6937</li>
+                        <li>lambdacrecheveterinaria@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
