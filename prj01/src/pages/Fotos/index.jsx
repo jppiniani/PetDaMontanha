cd0e3519c176e6bd0bd6f1fa08facddaee27dcb3
Nicolas <nick2007369@gmail.com>
2025-10-23 15:29:00 -0300

    Nicolas23-10/2

--- prj01/src/pages/Fotos/index.jsx
+++ prj01/src/pages/Fotos/index.jsx
@@ -7,6 +7,14 @@ export default function Fotos(){
         <>
         <ImagemComecoPagina titulo="Galeria" />
         <h2 className="titulo">Venha conhecer nosso trabalho mais de perto!</h2>
+        <div className="instagram">
+            <script async src="https://elfsightcdn.com/platform.js" />
+            <div
+                className="elfsight-app-74e876ca-b715-47e3-8dc3-f950dde34fef"
+                data-elfsight-app-lazy=""
+            />
+        </div>
+        <p>Aqui embaixo temos o carrossel que o João não quis ;(</p>
         <Carrossel />
         </>
     )
